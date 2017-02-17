fe02fb55f45cc03f6b9bb125099b59512972d97c
korakod.s <korakod.s@gmail.com>
2017-02-17 10:29:39 +0700

    update unallowcate

--- src/sbc/httpControl/HttpControlReq.go
+++ src/sbc/httpControl/HttpControlReq.go
@@ -17,10 +17,6 @@ import (
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/sessions"
 
-	//	"io"
-	//	"io/ioutil"
-
-	"github.com/ernado/sdp"
 	//	"os"
 	"strconv"
 	"sync"
@@ -31,11 +27,10 @@ import (
 // var OrigSdp OriginSdp
 
 type sdpMessages struct {
-	SDP             string
-	XSession        string `json:"x-session"`
-	CallbackAddr    string `json:"Callback-Address"`
-	CallbackSession string `json:"Callback-Session"`
-
+	SDP               string
+	XSession          string `json:"x-session"`
+	CallbackAddr      string `json:"Callback-Address"`
+	CallbackSession   string `json:"Callback-Session"`
 	Resultcode        string `json:"resultcode"`
 	Deverlopermessage string `json:"deverlopermessage"`
 }
@@ -172,7 +167,7 @@ func TestClient(w http.ResponseWriter, r *http.Request, session sessions.Session
 	//decode base64
 	desdp, _ := b64.StdEncoding.DecodeString(sdp.SDP)
 	log.Println("SDP Decode: ", string(desdp))
-	mediaDesc := sbpParser(desdp, aport, vport)
+	mediaDesc := SdpParser(desdp, aport, vport)
 	log.Println(mediaDesc.ip)
 
 	log.Println("Old port: ", mediaDesc.audio.port)
@@ -250,7 +245,7 @@ func TestClient2(w http.ResponseWriter, r *http.Request, session sessions.Sessio
 	//decode base64
 	desdp, _ := b64.StdEncoding.DecodeString(sdp.SDP)
 	log.Println("SDP Decode: ", string(desdp))
-	mediaDesc := sbpParser(desdp, aport, vport)
+	mediaDesc := SdpParser(desdp, aport, vport)
 
 	log.Println("Old port: ", mediaDesc.audio.port)
 	log.Println("New port: ", aport)
@@ -283,85 +278,7 @@ func TestClient2(w http.ResponseWriter, r *http.Request, session sessions.Sessio
 	fmt.Println(string(res2B))
 	fmt.Fprintf(w, string(res2B))
 }
-func sbpParser(sdpByte []byte, aport, vport string) OriginSdp {
-	var (
-		s   sdp.Session
-		err error
-	)
 
-	if s, err = sdp.DecodeSession(sdpByte, s); err != nil {
-		log.Fatal("err:", err)
-	}
-	// for k, v := range s {
-	// 	fmt.Println(k, v)
-	// }
-	d := sdp.NewDecoder(s)
-	m := new(sdp.Message)
-	if err = d.Decode(m); err != nil {
-		log.Fatal("err:", err)
-	}
-	log.Println("Decoded session", m.Name)
-	log.Println("Info:", m.Info)
-	log.Println("Origin:", m.Origin)
-	log.Println("IP 4: ", m.Origin.Address)
-	log.Println("IP 4: ", m.Timing)
-	log.Println("NetworkType: ", m.Connection.NetworkType)
-	log.Println("AddressType: ", m.Connection.AddressType)
-	log.Println("IP: ", m.Connection.IP)
-	log.Println("TTL: ", m.Connection.TTL)
-	log.Println("Addresses: ", m.Connection.Addresses)
-
-	orig := OriginSdp{}
-	medias1 := sdp.Medias{}
-
-	orig.ip = m.Origin.Address
-	isMultiMediaAudio := false
-	isMultiMediaVideo := false
-	// var isRemove []int
-	for i, media := range m.Medias {
-		log.Println("=======================")
-		log.Println("Type: ", media.Description.Type)
-		log.Println("Port: ", media.Description.Port)
-		log.Println("PortsNumber: ", media.Description.PortsNumber)
-		log.Println("Protocol: ", media.Description.Protocol)
-		log.Println("Format: ", media.Description.Format)
-		log.Println("Medias Connection: ", media.Connection)
-		log.Println("Medias Attributes: ", media.Attributes)
-		log.Println("Medias Encryption: ", media.Encryption)
-		log.Println("Medias Bandwidths: ", media.Bandwidths)
-
-		switch media.Description.Type {
-		case "audio":
-			if !isMultiMediaAudio {
-				orig.audio.port = media.Description.Port
-				orig.audio.portsNumber = media.Description.PortsNumber
-				orig.audio.protocal = media.Description.Protocol
-
-				isMultiMediaAudio = true
-				medias1 = append(medias1, m.Medias[i])
-			}
-		case "video":
-			if !isMultiMediaVideo {
-				orig.video.port = media.Description.Port
-				orig.video.portsNumber = media.Description.PortsNumber
-				orig.video.protocal = media.Description.Protocol
-
-				isMultiMediaVideo = true
-				medias1 = append(medias1, m.Medias[i])
-			}
-		}
-
-	}
-	// defining medias
-	m.Medias = medias1
-	//replace ip
-	//	m.Origin.Address = conf.Conf.Localip
-	m.Connection.IP = net.ParseIP(conf.Conf.Localip)
-	//encode base64
-	//	log.Print("dddddddddddddddddd", orig.audio.port)
-	return orig
-
-}
 func rtpMapping(session, uri, port string) {
 
 	log.SetFlags(log.Lshortfile)
@@ -404,35 +321,6 @@ func rtpMapping(session, uri, port string) {
 	log.Println("Session increment ", session, sbc)
 }
 
-func constructSdp(me *sdp.Message) string {
-	var (
-		s sdp.Session
-		b []byte
-	)
+func UnResoreceAllocate(w http.ResponseWriter, r *http.Request, session sessions.Session, ps martini.Params) {
 
-	// defining message
-	m := me
-	//	m.AddFlag("nortpproxy:yes")
-
-	// appending message to session
-	s = m.Append(s)
-
-	// appending session to byte buffer
-	b = s.AppendTo(b)
-	log.Println("SDP Construct :", string(b)+"\n")
-	return string(b) + "\n"
-}
-
-type OriginSdp struct {
-	ip    string
-	audio struct {
-		port        int
-		portsNumber int
-		protocal    string
-	}
-	video struct {
-		port        int
-		portsNumber int
-		protocal    string
-	}
 }
